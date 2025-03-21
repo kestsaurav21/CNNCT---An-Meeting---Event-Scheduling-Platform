2e9b872115185b90a7365454d41a3da7e50a22ff
kestsaurav21 <kestsaurav21@outlook.com>
2025-03-21 22:14:48 +0530

    update: HeroSection

--- client/src/components/HeroSection/index.jsx
+++ client/src/components/HeroSection/index.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import hero from '../../assets/hero-image.png';
 import './style.css'
 
-const HeroSection = ({ title, cta, text, description }) => {
+const HeroSection = ({ title, cta, text, description, imageUrl }) => {
   return (
     <div className='hero-section'>
         <h1 className='title'>{title}</h1>
         <button className='primary-button'>{cta}</button>
-        <img className='hero-image' src={hero} alt="hero-image" />
+        <img className='hero-image' src={!imageUrl || hero} alt="hero-image"  />
         <p className='hero-text'>{text}</p>
         <p className='hero-description'>{description}</p>
     </div>
