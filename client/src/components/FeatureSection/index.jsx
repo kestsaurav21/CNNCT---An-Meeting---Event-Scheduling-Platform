7f64cd41030b206a39cde9498610828eaf06ac71
kestsaurav21 <kestsaurav21@outlook.com>
2025-03-21 15:49:00 +0530

    feat: add FeatureSection component

---
+++ client/src/components/FeatureSection/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './style.css'
+import featureImage from '../../assets/feature-image.png';
+
+const FeatureSection = ({ features }) => {
+  return (
+    <div className='feature-section'>
+        <section className="features-text">
+        {features.map((feature, index) => (
+            <div key={index} className="feature">
+            <h2 className='feature-title'>{feature.title}</h2>
+            <ul>
+                {feature.points.map((point, i) => (
+                <li key={i}>{point}</li>
+                ))}
+            </ul>
+            </div>
+        ))}
+        </section>
+        <section className="features-image">
+            <img className="image" src={featureImage} alt="feature-image" />
+        </section>
+
+    </div>
+
+    
+  )
+}
+
+export default FeatureSection
